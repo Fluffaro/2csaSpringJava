432d2ffa378ed70dbf9ccb911ab9757ac429ca14
Fluffaro <45957559+Fluffaro@users.noreply.github.com>
2021-12-13 23:34:20 +0800

    update and delete

--- src/main/java/com/csa/samplefullstack/controller/EmployeeController.java
+++ src/main/java/com/csa/samplefullstack/controller/EmployeeController.java
@@ -4,29 +4,46 @@ import com.csa.samplefullstack.entity.Employee;
 import com.csa.samplefullstack.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
 @CrossOrigin("*")
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @RequestMapping("/employees")
-    public List<Employee> getAllEmployees(){
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
 
     }
+
     @PostMapping("/employees")
-    public Employee postEmployee(@RequestBody Employee employee){
+    public Employee postEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
 
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeFromID(@PathVariable long id) {
+        return employeeRepository.findById(id).get();
+    }
 
+    @PutMapping("/employees")
+    public Employee putEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id) {
+        employeeRepository.deleteById(id);
+        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+    }
 }
